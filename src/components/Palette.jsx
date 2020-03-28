9bb5f427e943b16485d2e5fb448a1c085a35085f
Daniel <daniellanguilller@gmail.com>
2020-03-28 20:46:49 +1100

    28 mar

--- src/components/Palette.jsx
+++ src/components/Palette.jsx
@@ -1,15 +1,42 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "../styles/Palette.css";
+import Navbar from "./Navbar";
 
 export default class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      level: 500,
+      format: "hex"
+    };
+  }
+
+  changeLevel = level => {
+    this.setState({
+      level
+    });
+  };
+
+  changeFormat = val => {
+    this.setState({
+      format: val
+    });
+  };
+
   render() {
-    const colorBoxes = this.props.colors.map(color => {
-      return <ColorBox background={color.color} name={color.name} />;
+    const { colors } = this.props.palette;
+    const { level, format } = this.state;
+    const colorBoxes = colors[level].map(color => {
+      return <ColorBox background={color[format]} name={color.name} />;
     });
     return (
       <div className="Palette">
-        {/* Navbar goes here */}
+        <Navbar
+          level={level}
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
         <div className="Palette-colors">{colorBoxes}</div>
         {/* footer eventually */}
       </div>
