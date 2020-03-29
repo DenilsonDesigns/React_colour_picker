241fc6147ec4c951d4b4fbf972658c9c2b34275b
Daniel <daniellanguilller@gmail.com>
2020-03-29 21:29:44 +1100

    adding cjss styles

--- src/components/PaletteList.jsx
+++ src/components/PaletteList.jsx
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import MiniPalette from "./MiniPalette";
 
 export default class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
       <div>
+        <MiniPalette />
+        <h1>React Colors</h1>
         {palettes.map(pal => {
-          return <Link to={`/palette/${pal.id}`}>{pal.paletteName}</Link>;
+          //   return <Link to={`/palette/${pal.id}`}>{pal.paletteName}</Link>;
+          return <MiniPalette {...pal} />;
         })}
       </div>
     );
